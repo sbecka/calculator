09686958c2fe6ad6f6d1eb33908529ab11d11f73
Sabrina Becker <sbecka8@gmail.com>
2020-05-20 14:54:56 -0400

    Use sendFile to get html and configure post on form to add 2 numbers

--- calculator.js
+++ calculator.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => 
-    res.send(`<h1>Calculator</h1>`)
-);
+app.use(bodyParser.urlencoded({extended: true})); // parse data from a form
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+    // console.log(__dirname); // C:\Users\calculator
+});
+// __dirname is file path of the current file no matter where it's hosted, on another computer for example
+
+app.post('/', (req, res) => {
+    const { num1, num2 } = req.body;
+    const sum = Number(num1) + Number(num2);
+    res.send(`Sum: ${sum} Thanks for posting that!`);
+})
 
 app.listen(3000, () => console.log('Server listening on port 3000'));
